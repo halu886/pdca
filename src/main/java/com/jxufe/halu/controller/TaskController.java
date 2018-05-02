d6d1b4805e6edbffcdb0e84636170735dd89432d
halu <halu886@qq.com>
2018-05-02 21:18:59 +0800

    修复task模块bug

--- src/main/java/com/jxufe/halu/controller/TaskController.java
+++ src/main/java/com/jxufe/halu/controller/TaskController.java
@@ -105,7 +105,7 @@ public class TaskController {
     @RequestMapping(value = "/add/{type}", method = RequestMethod.POST)
     public @ResponseBody
     Map addTask(@RequestBody Map body,@PathVariable("type") String type,HttpServletRequest request) {
-        String projectId  = (String) request.getAttribute("projectId");
+        String projectId  = "";
         Map<String, Object> result = new HashMap<String, Object>();
         result.put("status", false);
         result.put("message", "添加失败");
@@ -116,59 +116,94 @@ public class TaskController {
             Task typeTask = new Task();
             int increateRs = 0;
             switch (type.toLowerCase()) {
-                case "t":
+                case "T":
                     if (service.isHasChild(pTaskId)) {
                         break;
                     }
                     String nameKey = "TypeTaskName";
                     String descriptionKey = "TypeTaskDescription";
-                    String[] typeList = {"p", "d", "c", "a"};
+                    String[] typeList = {"P", "D", "C", "A"};
                     List<Task> taskList = new ArrayList<Task>();
-                    for (int i = 0; i < 4; i++) {
-                        Task task = new Task();
-                        String taskOfType = typeList[i];
-                        String name = taskOfType + nameKey;
-                        String description = taskOfType + descriptionKey;
-                        String startDateKey = taskOfType + "TypeStartDate";
-                        long startDateLong = DateUtil.date2TimeStampLong((String) body.get(startDateKey), "yyyy-MM-dd HH:mm:ss");
-                        Timestamp startTime = new Timestamp(startDateLong);
-                        String endDateKey = taskOfType + "TypeEndDate";
-                        long endDateLong = DateUtil.date2TimeStampLong((String) body.get(endDateKey), "yyyy-MM-dd HH:mm:ss");
-                        Timestamp endTime = new Timestamp(endDateLong);
-                        task.setTaskName((String) body.get(name));
-                        task.setDescription((String) body.get(description));
-                        task.setTaskType(typeList[i]);
-                        task.setPTaskId(pTaskId);
-                        task.setStartDate(startTime);
-                        task.setEndDate(endTime);
-                        task.setCreateDate(new Timestamp(System.currentTimeMillis()));
-                        task.setProjectId(projectId);
-                        taskList.add(task);
+                    ArrayList bodyTypeTasks = (ArrayList)body.get("typeTask");
+                    for (int i = 0; i < 4; ++i) {
+                        typeTask = new Task();
+                        Map typeTaskMap = (Map) bodyTypeTasks.get(i);
+//                        Task task = new Task();
+//                        String taskOfType = typeList[i];
+//                        String name = taskOfType + nameKey;
+//                        String description = taskOfType + descriptionKey;
+//                        String startDateKey = taskOfType + "TypeStartDate";
+//                        long startDateLong = DateUtil.date2TimeStampLong((String) body.get(startDateKey), "yyyy-MM-dd HH:mm:ss");
+//                        Timestamp startTime = new Timestamp(startDateLong);
+//                        String endDateKey = taskOfType + "TypeEndDate";
+//                        long endDateLong = DateUtil.date2TimeStampLong((String) body.get(endDateKey), "yyyy-MM-dd HH:mm:ss");
+//                        Timestamp endTime = new Timestamp(endDateLong);
+//                        task.setTaskName((String) body.get(name));
+//                        task.setDescription((String) body.get(description));
+//                        task.setTaskType(typeList[i]);
+//                        task.setPTaskId(pTaskId);
+//                        task.setStartDate(startTime);
+//                        task.setEndDate(endTime);
+//                        task.setCreateDate(new Timestamp(System.currentTimeMillis()));
+//                        task.setProjectId(projectId);
+//                        taskList.add(task);
+                        typeTask.setTaskType(((String) typeTaskMap.get("type")).toUpperCase());
+                        typeTask.setCreateDate(DateUtil.getCurrentTimestamp());
+                        typeTask.setProjectId((String) typeTaskMap.get("projectId"));
+                        typeTask.setProgress("0");
+                        typeTask.setDescription((String) typeTaskMap.get("description"));
+                        typeTask.setStartDate(new Timestamp(DateUtil.date2TimeStampLong((String)typeTaskMap.get("startDate"),"yyyy-MM-dd HH:mm:ss")));
+                        typeTask.setEndDate(new Timestamp(DateUtil.date2TimeStampLong((String)typeTaskMap.get("endDate"),"yyyy-MM-dd HH:mm:ss")));
+                        typeTask.setTaskName((String) typeTaskMap.get("name"));
+                        typeTask.setNodeProgress(typeTaskMap.get("progress")+"");
+                        typeTask.setPTaskId((String) typeTaskMap.get("pTaskId"));
+                        taskList.add(typeTask);
                     }
                     increateRs = service.increateTypeTask(taskList);
                     if (increateRs != 4) {
                         throw new Exception("添加异常");
                     }
                     break;
-                case "p":
-                case "d":
-                case "c":
-                case "a":
+                case "P":
+                case "D":
+                case "C":
+                case "A":
                     long startDateLong = DateUtil.date2TimeStampLong((String) body.get("startDate"), "yyyy-MM-dd HH:mm:ss");
                     Timestamp startTime = new Timestamp(startDateLong);
                     long endDateLong = DateUtil.date2TimeStampLong((String) body.get("endDate"), "yyyy-MM-dd HH:mm:ss");
+                    projectId = (String)body.get("projectId");
                     Timestamp endTime = new Timestamp(endDateLong);
                     typeTask.setTaskType("T");
                     typeTask.setDescription((body.get(childDescriptionKey) == null) ? "" : (String) body.get(childNameKey));
                     typeTask.setTaskName((String) body.get(childNameKey));
                     typeTask.setProjectId(projectId);
-                    typeTask.setPTaskId(pTaskId);
+                    typeTask.setPTaskId((String)body.get("pTaskId"));
+                    typeTask.setTaskName((String) body.get("name"));
+                    typeTask.setProgress("0");
+                    typeTask.setNodeProgress((String) body.get("progress"));
+                    typeTask.setDescription((String) body.get("description"));
                     typeTask.setStartDate(startTime);
                     typeTask.setEndDate(endTime);
                     typeTask.setCreateDate(new Timestamp(System.currentTimeMillis()));
                     typeTask.setTno(service.numChild(pTaskId) + 1);
-                    typeTask.setProjectId(projectId);
-                    service.increateStepTask(typeTask);
+                    service.addTask(typeTask);
+                    break;
+                case"r":
+                    Task rootTask = new Task();
+                    projectId = (String) body.get("projectId");
+                    rootTask.setTaskType("T");
+                    rootTask.setCreateDate(DateUtil.getCurrentTimestamp());
+                    rootTask.setProjectId(projectId);
+                    rootTask.setProgress("0");
+                    rootTask.setDescription((String) body.get("description"));
+                    int rootTaskCount = service.countRootTaskByProjectId(projectId);
+                    rootTask.setTno(++rootTaskCount);
+                    rootTask.setTno(rootTaskCount);
+                    rootTask.setStartDate(new Timestamp(DateUtil.date2TimeStampLong((String)body.get("startDate"),"yyyy-MM-dd HH:mm:ss")));
+                    rootTask.setEndDate(new Timestamp(DateUtil.date2TimeStampLong((String)body.get("endDate"),"yyyy-MM-dd HH:mm:ss")));
+                    rootTask.setTaskName((String) body.get("name"));
+                    rootTask.setNodeProgress((String) body.get("progress"));
+                    service.addTask(rootTask);
                     break;
                 default:
                     throw new Exception("任务类型异常");
@@ -207,7 +242,7 @@ public class TaskController {
 //        return result;
 //    }
 
-    @RequestMapping(value = "/overTask/{taskId}", method = RequestMethod.GET)
+    @RequestMapping(value = "/overTask/{taskId}", method = RequestMethod.PUT)
     public @ResponseBody
     Map overTask(@PathVariable("taskId") String taskId) {
         Map<String, Object> result = new HashMap<String, Object>();
@@ -225,6 +260,25 @@ public class TaskController {
             result.put("message", "结束异常");
         }
         return result;
+    }
+
+    @RequestMapping(value = "deleteNodeById",method = RequestMethod.DELETE)
+    public  @ResponseBody
+    Map deleteNodeById(HttpServletRequest req){
+        Map result = new HashMap();
+        result.put("status",false);
+        result.put("message","删除失败");
+        try{
+            String taskId = (String) req.getParameter("taskId");
+            int deleteNum = service.deleteNodeByTaskId(taskId);
+            result.put("data",deleteNum);
+            result.put("status",true);
+            result.put("message","删除成功");
+        }catch (Exception e){
+            e.printStackTrace();
+            result.put("message","删除异常");
+        }
+         return  result;
     }
 
     @RequestMapping("/chart/countAddTaskWeek")
